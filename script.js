0bbc4e28f5c319c278f41ccd2d6f608bf6d23e99
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-18 11:46:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+    var boldWords = document.querySelectorAll("#paragraph strong");
+    for (var i = 0; i < boldWords.length; i++) {
+        boldWords[i].style.color = "green";
+    }
 }
 
 
 function return_normal() {
     //Write your code here
-
+    var boldWords = document.querySelectorAll("#paragraph strong");
+    for (var i = 0; i < boldWords.length; i++) {
+        boldWords[i].style.color = "black";
+    }
     
 }
